d523bf5df06fa66f53fa4a22fb6603e7914a408b
Eugene <help@gmc24.org>
2017-11-07 21:32:01 +0300

    First-lesson-homework

---
+++ JS-Basics/lesson-01/js/script.js
@@ -0,0 +1,16 @@
+var Tc = +prompt("Введите температуру по Цельсию: ");
+var Tf = 0;
+
+Tf = (9/5)*Tc + 32;
+
+alert (Tf);
+console.log(Tc+' градусов С = '+Tf+' по Фаренгейту');
+
+var name = 'Василий';
+var  admin = name;
+console.log(admin);
+
+// выражение будет равно 1000108, проверяем
+console.log(1000+'108');
+
+
