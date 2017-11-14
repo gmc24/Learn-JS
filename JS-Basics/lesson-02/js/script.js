3bdd07566c8656b44932ccfe4589d2673b17f865
gmc24 <impress@yandex.ru>
2017-11-14 08:29:44 +0300

    lesson-2-7

--- JS-Basics/lesson-02/js/script.js
+++ JS-Basics/lesson-02/js/script.js
@@ -23,34 +23,75 @@ alert (x);*/
 
 // №3//
 function getRandomInt(min, max) {
-    return Math.floor(Math.random()*(max - min +1))
+    return Math.floor(Math.random() * (max - min)) + min;
 }
-var a = getRandomInt(1,100);
-var b = getRandomInt(1,100);
+
+/*var a = getRandomInt(-100,100);
+var b = getRandomInt(-100,100);
 
 alert (a+'   '+b);
 
-// Объявить две целочисленные переменные a и b и задать им произвольные начальные значения.
+if (a>=0 && b>=0) {
+    alert(a-b);
+} else if (a<0 && b<0) {
+    alert(a*b);
+} else  {
+    alert(a+b);
+}*/
+
+// №4 //
+/*var a2 = getRandomInt(0,15);
+for (var i=a2; i<=15;i++) {
+switch (true) {
+       case i<=15:
+           alert(i);
+           break;
+   }
+}*/
+
+// №5 //
+function sum (x,y) {
+    return sum=x+y;
+}
+
+function subtr (x,y) {
+    return subtr=x-y;
+}
+
+function division (x,y) {
+    return division=x/y;
+}
+
+function multiply (x,y) {
+    return multiply=x*y;
+}
+
+// №6 //
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case 'sum':
+            return sum(arg1,arg2);
+            break;
+        case 'subtr':
+            return subtr(arg1,arg2);
+            break;
+        case 'division':
+            return division(arg1,arg2);
+            break;
+        case 'multiply':
+            return multiply(arg1,arg2);
+            break;
+    }
+}
+alert(mathOperation(3,5,'multiply'));
+
+// №7 //
 /*
-    Затем написать скрипт, который работает по следующему принципу:
-    если a и b положительные, вывести их разность;
-если а и b отрицательные, вывести их произведение;
-если а и b разных знаков, вывести их сумму; (ноль можно считать положительным числом)
+нельзя сравнить null и 0
+0 - это значение, null означает отсутствие какого-либо значения
+*/
 
-Присвоить переменной а значение в промежутке [0..15].
-С помощью оператора switch организовать вывод чисел от a до 15.
-
-    Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.
-        Обязательно использовать оператор return.
-
-    Реализовать функцию с тремя параметрами:
-        function mathOperation(arg1, arg2, operation)
-    где arg1, arg2 – значения аргументов, operation – строка с названием операции.
-
-        В зависимости от переданного значения операции выполнить одну из арифметических операций (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).
-
-        7*. Сравнить null и 0. Попробуйте объяснить результат.
-        8*. С помощью рекурсии организовать функцию факториала.
+/*8*. С помощью рекурсии организовать функцию факториала.
             Факторииал числа – это число, умноженное на «себя минус один», затем на «себя минус два» и так далее, до единицы. Обозначается n!
             n! = n * (n - 1) * (n - 2) * ...*1
             */
