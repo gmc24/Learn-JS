e88db221dae4ce175cdaf2160e3dee26fb739014
gmc24 <impress@yandex.ru>
2017-11-18 01:28:00 +0300

    дз3 готово

--- JS-Basics/lesson-03/js/script.js
+++ JS-Basics/lesson-03/js/script.js
@@ -1,6 +1,7 @@
 "use strict"
 
 // 1
+/*
 var i = 0;
 var arr=[];
 var n = +prompt('До какого натурального N ищем простые числа? ');
@@ -28,6 +29,44 @@ while (i<n) {
     if (arr[i]) {console.log(i);}
     i++;
 }
+*/
 
 
 // 2
+
+/*
+var i=0;
+do {
+if (i==0) {console.log(i+' - это ноль');}
+else if (i%2===0) {console.log(i+' - четное число');}
+else {console.log(i+' - нечетное число');}
+i++;
+} while (i<=10);
+*/
+
+// 3
+//for (var i=0; i<10;console.log(i++)) {}
+
+//4
+/*
+for (var i = 1; i<=20;i++) {
+    var row='';
+    for (var j=0; j<i;j++) { row+='x' }
+    console.log(row);
+}
+*/
+// с методом repeat()
+//for (var i = 1; i<=20;i++) { console.log('x'.repeat(i))}
+
+
+//5 - усовершенствуем пирамидку
+/*var n = +prompt("Сколько рядов будет у пирамиды?");
+for (var i = 0; i<n;i++) {
+    var row='';
+    for (var j=1; j<=(n-i);j++) { row+=' '};
+    for (var j=1; j<=(i*2+1);j++) { row+='x'};
+    console.log(row);
+}*/
+// с методом repeat()
+var n = +prompt("Сколько рядов будет у пирамиды?");
+for (var i = 0; i<n;i++) { console.log(' '.repeat(n-i)+'x'.repeat(2*i+1)) }
